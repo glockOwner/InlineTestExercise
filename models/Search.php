9d272f3b5e5df1658610bbf65ed4ddbdb442c852
glock.owner <danil.avdeev.2004@gmail.com>
2022-09-04 15:37:01 +0400

    search without word boundary

--- models/Search.php
+++ models/Search.php
@@ -5,7 +5,7 @@ class Search
     public static function search($searchInput)
     {
         $db = Db::getConnection();
-        $sql = "SELECT comments.body AS 'comment_body', posts.title AS 'post_title' FROM `comments` INNER JOIN `posts` ON comments.postId = posts.id WHERE comments.body RLIKE '\\\b$searchInput\\\b'";
+        $sql = "SELECT comments.body AS 'comment_body', posts.title AS 'post_title' FROM `comments` INNER JOIN `posts` ON comments.postId = posts.id WHERE comments.body LIKE '%$searchInput%'";
         $result = $db->query($sql);
         $posts = array();
         while ($row = $result->fetch()) {
